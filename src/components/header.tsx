bc0378a49e87e1fe21ee5f681aafb3a72335e00d
Aman Maurya <amanmaurya112233@gmail.com>
2025-06-22 09:46:01 +0530

    Nav link fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ const navLinks = [
   { name: "Home", href: "#home" },
   { name: "About", href: "#about" },
   { name: "Projects", href: "#projects" },
-  { name: "Education", href: "#education" },
+  { name: "Timeline", href: "#education" },
   { name: "Contact", href: "#contact" },
 
 ]
